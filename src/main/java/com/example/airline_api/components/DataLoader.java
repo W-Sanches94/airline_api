9d2a964f91ac4105a4e026946841a40e60370feb
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-02-16 17:04:49 +0000

    Passenger Controller complete

---
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -0,0 +1,35 @@
+//package com.example.airline_api.components;
+//
+//import com.example.airline_api.repositories.FlightRepository;
+//import com.example.airline_api.repositories.PassengerRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DataLoader implements ApplicationRunner {
+//
+//    @Autowired
+//    FlightRepository flightRepository;
+//
+//    @Autowired
+//    PassengerRepository passengerRepository;
+//
+//    public DataLoader(){
+//
+//    }
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        //
+//
+//
+//        //
+//
+//
+//        //
+//
+//        //
+//    }
+//}
--- src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,6 +1,9 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +13,28 @@ import java.util.List;
 @RequestMapping("/passengers")
 public class PassengerController {
 
+    // INDEX
+    @Autowired
+    PassengerService passengerService;
+
     // Display details of all passengers
     @GetMapping
     public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+        return new ResponseEntity<>(passengerService.findAllPassengers(), HttpStatus.OK);
     }
 
     // Display specific passenger details
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable Long id){
+        Passenger foundPassenger = passengerService.findPassenger(id);
+        return new ResponseEntity<>(foundPassenger, HttpStatus.OK);
     }
 
     // Add a new passenger
-    @PostMapping
-    public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
+    @PutMapping
+    public ResponseEntity<Passenger> addNewPassenger(@RequestBody Passenger passenger){
+        passengerService.savePassenger(passenger);
+        return new ResponseEntity<>(passenger,HttpStatus.CREATED);
     }
 
 }
